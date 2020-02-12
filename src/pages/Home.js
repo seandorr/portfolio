ea409ea091ae4e6c9a21f221abb67b5f4fe4a3ca
Sean Dorr <seanmichael.dorr@gmail.com>
2020-02-12 20:48:32 +0000

    Refactored project code to be templatized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import ProjectGridItem from './ProjectGridItem';
 
 const Home = () => {
 	return (
-		<>
-			homepage
-		</>
+		<div className="grid-container">
+			<ProjectGridItem 
+				id={"1"}
+				companyTitle={"Gemini Diversified Services"}
+				serviceOne={"Logo & Branding"}
+				serviceTwo={"Web Design & Dev"}
+				image={"/images/gemini-hero.jpg"}
+			/>
+			<ProjectGridItem 
+				id={"2"}
+				companyTitle={"Eye Medical Clinic"}
+				serviceOne={"Web Design"}
+				serviceTwo={"Iconography"}
+				image={""}
+			/>
+			<ProjectGridItem 
+				id={"3"}
+				companyTitle={"codeBase"}
+				serviceOne={"WIP"}
+				serviceTwo={""}
+				image={""}
+			/>
+			<ProjectGridItem 
+				id={"4"}
+				companyTitle={"Los Dos Creative"}
+				serviceOne={"Logo Design"}
+				serviceTwo={"Branding"}
+				image={""}
+			/>
+			<ProjectGridItem 
+				id={"5"}
+				companyTitle={"Targetbase"}
+				serviceOne={"Web Design"}
+				serviceTwo={"Front-End Dev"}
+				image={""}
+			/>
+			<ProjectGridItem 
+				id={"6"}
+				companyTitle={"Miller Public Relations"}
+				serviceOne={"Social Posts"}
+				serviceTwo={"Custom Illustrations"}
+				image={""}
+			/>
+		</div>
 	);
 }
 

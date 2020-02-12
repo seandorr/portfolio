6f35a20be72ea145c3470038f170ec072e33e57f
Sean Dorr <sdorr@gemmas-imac.local>
2020-02-12 08:55:26 +0000

    Creating routing for navigation thru app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Borders from './Borders';
 import NavBar from './NavBar';
+import Home from './pages/Home';
+import Wip from './pages/Wip';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
-const App = () => {
-	return (
-		<>
-			<Borders />
-			<NavBar />
-		</>	
-	);
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+class App extends Component {
+	render() {
+		return (
+			<>
+				<Borders />
+				<NavBar />
+				
+				<div id="page-content">
+					<BrowserRouter>
+						<Switch>
+							<Route 
+								exact 
+								path="/" 
+								component={Home} 
+							/>
+							<Route 
+								path="/wip" 
+								component={Wip} 
+							/>
+							<Route 
+								path="/about" 
+								component={About} 
+							/>
+							<Route 
+								path="/contact" 
+								component={Contact} 
+							/>
+						</Switch>
+					</BrowserRouter>
+				</div>
+			</>
+		);
+	}
 }
 
 export default App;

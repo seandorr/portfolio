8152c46f983eadaf3b8a24357f1da3fe4d96b134
Sean Dorr <sdorr@gemmas-imac.local>
2020-02-12 07:03:48 +0000

    Creating borders component

---
+++ src/Borders.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Borders = () => {
+	return (
+		<div id="page-borders">
+			<div id="top" class="green-border">
+				<div id="top-border"></div>
+			</div>
+			<div id="right" class="green-border">
+				<div id="right-border"></div>
+			</div>
+			<div id="bottom" class="green-border">
+				<div id="bottom-border"></div>
+			</div>
+			<div id="left" class="green-border">
+				<div id="left-border"></div>
+			</div>
+		</div>
+	);
+}
+
+export default Borders;

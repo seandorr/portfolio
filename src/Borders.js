613c5a3420300cde0140dfb16160da3ad33d0c8f
Sean Dorr <seanmichael.dorr@gmail.com>
2020-02-12 20:47:31 +0000

    Changed html syntac to JSX

--- src/Borders.js
+++ src/Borders.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Borders = () => {
 	return (
 		<div id="page-borders">
-			<div id="top" class="border green-border">
+			<div id="top" className="border green-border">
 				<div id="top-border"></div>
 			</div>
-			<div id="right" class="border green-border">
+			<div id="right" className="border green-border">
 				<div id="right-border"></div>
 			</div>
-			<div id="bottom" class="border green-border">
+			<div id="bottom" className="border green-border">
 				<div id="bottom-border"></div>
 			</div>
-			<div id="left" class="border green-border">
+			<div id="left" className="border green-border">
 				<div id="left-border"></div>
 			</div>
 		</div>

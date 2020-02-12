c835ca2d7e93206acc68c123313c4af5b6cf46c4
Sean Dorr <sdorr@gemmas-imac.local>
2020-02-12 08:56:16 +0000

    Changing links so that navigation works

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
 	return (
 		<nav id="nav-bar">
-			<a href="https://seandorr.com" className="logo-link">
+			<a href="/" className="logo-link">
 				<div className="logo" id="firstName"><h1>Sean</h1></div>
 				<ul id="logo-list">
 					<li><div className="door"></div><div className="knob"></div></li>
@@ -12,9 +12,9 @@ const NavBar = () => {
 			</a>
 			<ul className="nav-bar-links" id="nav-bar-ul">
 				<li><a className="underline active-underline mobile-only" href="https://seandorr.com">Projects</a></li>
-				<li><a className="underline" href="#">WIP</a></li>
-				<li><a className="underline" href="#">About</a></li>
-				<li><a className="underline" href="#">Contact</a></li>
+				<li><a className="underline" href="/Wip">WIP</a></li>
+				<li><a className="underline" href="/About">About</a></li>
+				<li><a className="underline" href="/Contact">Contact</a></li>
 			</ul>
 				<a href="javascript:void(0);" className="nav-bar-icon" onclick="mobileNavBar()"></a>
 		</nav>

14a6fa950816db34977d26bcc9d16d5b73b4e60a
Sean Dorr <sdorr@Gemmas-iMac.local>
2020-02-13 18:07:04 +0100

    Changing const to class / working on hover

--- src/pages/ProjectGridItem.js
+++ src/pages/ProjectGridItem.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
-const ProjectGrid = (props) => {
-	// function changeProj1Border() {
-	// 	document.getElementById("top").style.backgroundColor = "#0FA5A1";
-	// 	document.getElementById("right").style.backgroundColor = "#0FA5A1";
-	// 	document.getElementById("bottom").style.backgroundColor = "#0FA5A1";
-	// 	document.getElementById("left").style.backgroundColor = "#0FA5A1";
-	// }
-	
-	return (
-		<a className="grid-item" id={`grid-item-${props.id}`} href="#" onmouseover={`changeProj${props.id}Border()`} onmouseout="changeBorderBack()">
-			<h1 className="company-title">{props.companyTitle}</h1>
-			<h1 className="hidden">{props.serviceOne}<br />+<br />{props.serviceTwo}</h1>
-			<img id={`grid-project-${props.id}`} src={props.image} />
-		</a>
-	);
+class ProjectGrid extends React.Component {
+	render() {
+		return (
+			<a className="grid-item" id={`grid-item-${this.props.id}`} href="#" onMouseEnter={() => this.props.changeProj1Border()} onMouseLeave="changeBorderBack()">
+				<h1 className="company-title">{this.props.companyTitle}</h1>
+				<h1 className="hidden">{this.props.serviceOne}<br />+<br />{this.props.serviceTwo}</h1>
+				<img id={`grid-project-${this.props.id}`} src={this.props.image} />
+			</a>
+		);
+	}
 }
 
 export default ProjectGrid;

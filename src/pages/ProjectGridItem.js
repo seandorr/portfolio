519009bebb237d45be07c3c35a823546af9922e5
Sean Dorr <sdorr@Gemmas-iMac.local>
2020-02-19 10:19:06 +0100

    Getting hover funcionality on HP to work thru props

--- src/pages/ProjectGridItem.js
+++ src/pages/ProjectGridItem.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-class ProjectGrid extends React.Component {
+class ProjectGridItem extends React.Component {
+
+	changeBorder() {
+		document.getElementById("top").style.backgroundColor = this.props.border
+		document.getElementById("right").style.backgroundColor = this.props.border
+		document.getElementById("bottom").style.backgroundColor = this.props.border
+		document.getElementById("left").style.backgroundColor = this.props.border
+	}
+
+	changeBorderBack() {
+		document.getElementById("top").style.backgroundColor = '#62e6ac'
+		document.getElementById("right").style.backgroundColor = '#62e6ac'
+		document.getElementById("bottom").style.backgroundColor = '#62e6ac'
+		document.getElementById("left").style.backgroundColor = '#62e6ac'
+	}
+
 	render() {
 		return (
-			<a className="grid-item" id={`grid-item-${this.props.id}`} href="#" onMouseEnter={() => this.props.changeProj1Border()} onMouseLeave="changeBorderBack()">
+			<a className="grid-item" id={`grid-item-${this.props.id}`} href="#" onMouseOver={() => this.changeBorder()} onMouseOut={() => this.changeBorderBack()}>
 				<h1 className="company-title">{this.props.companyTitle}</h1>
 				<h1 className="hidden">{this.props.serviceOne}<br />+<br />{this.props.serviceTwo}</h1>
 				<img id={`grid-project-${this.props.id}`} src={this.props.image} />
@@ -12,4 +27,4 @@ class ProjectGrid extends React.Component {
 	}
 }
 
-export default ProjectGrid;
+export default ProjectGridItem;

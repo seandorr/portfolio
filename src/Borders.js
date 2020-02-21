dea73667de976bbde06baea68bd04bb59c397741
Sean Dorr <sdorr@Gemmas-iMac.local>
2020-02-21 08:32:42 +0100

    Fixing border color issues and classes

--- src/Borders.js
+++ src/Borders.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Borders = () => {
 	return (
 		<div id="page-borders">
-			<div id="top" className="border green-border">
+			<div id="top" className="border">
 				<div id="top-border"></div>
 			</div>
-			<div id="right" className="border green-border">
+			<div id="right" className="border">
 				<div id="right-border"></div>
 			</div>
-			<div id="bottom" className="border green-border">
+			<div id="bottom" className="border">
 				<div id="bottom-border"></div>
 			</div>
-			<div id="left" className="border green-border">
+			<div id="left" className="border">
 				<div id="left-border"></div>
 			</div>
 		</div>

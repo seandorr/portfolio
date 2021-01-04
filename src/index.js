517d3d37a2e4d1bee1eb08df78bc8f6a5ed5dfc2
Sean Dorr <sean@nominapress.es>
2021-01-04 21:49:25 +0100

    adding i18 logic for translating app

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import App from "./App";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 import "./i18n";
-import common_en from "./translations/en/common.json";
-import common_es from "./translations/es/common.json";
+import home_en from "./translations/en/home.json";
+import home_es from "./translations/es/home.json";
 
 i18next.init({
   interpolation: { escapeValue: false },
   lng: "en",
   resources: {
     en: {
-      common: common_en,
+      home: home_en,
     },
     es: {
-      common: common_es,
+      home: home_es,
     },
   },
 });

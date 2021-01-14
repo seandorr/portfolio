f58ce3128eaa75cce116203f76862d3d9690bda7
Sean Dorr <sean@nominapress.es>
2021-01-14 11:20:15 +0100

    Calling GetProjectColor to pass projectName to function

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,11 @@ import React, { Suspense } from "react";
 import ProjectPreview from "../../components/projectPreview/ProjectPreview";
 import projectData from "./utils/constants/projectData";
 import "./home.scss";
+import GetProjectColor from "../../utils/functions/GetProjectColor";
 
 const Home = (props) => {
   const { translation } = props;
+
   return (
     <Suspense fallback="loading">
       {/* <div className="hero">
@@ -14,6 +16,8 @@ const Home = (props) => {
         </div>
       </div> */}
       {Object.values(projectData).map((projects) => {
+        const { projectName } = projects;
+        GetProjectColor(projectName);
         return <ProjectPreview projects={projects} translation={translation} />;
       })}
     </Suspense>

37513c6cc963d49715eae23a8de1850578e835c9
Sean Dorr <sean@nominapress.es>
2021-01-16 19:43:55 +0100

    Creating dynamic logic to get project color
    Moving function logic to this level

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import GridLines from "./components/gridLines/GridLines";
 import NavBar from "./components/navigation/NavBar";
 import Home from "./pages/home/Home";
-import GetProjectColor from "./utils/functions/GetProjectColor";
+import projectData from "./pages/home/utils/constants/projectData";
+
+const PROJECTS = Object.values(projectData);
 
 const App = () => {
   const [translation, i18n] = useTranslation("home");
   const [activeTranslationBtn, setActiveTranslationBtn] = useState("english");
+  const [activeProjectColor, setActiveProjectColor] = useState(
+    PROJECTS[0].projectColor
+  );
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleOnClickSpanishBtn = () => {
     i18n.changeLanguage("es");
@@ -19,19 +43,38 @@ const App = () => {
     setActiveTranslationBtn("english");
   };
 
-  const projectColor = GetProjectColor();
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  const calculatedWindowHeight = windowSize.height / 1.5;
+
+  const handleScroll = () => {
+    const indexOfProjectCurrentlyInView = Math.floor(
+      window.scrollY / calculatedWindowHeight
+    );
+
+    if (indexOfProjectCurrentlyInView < PROJECTS.length) {
+      setActiveProjectColor(
+        PROJECTS[indexOfProjectCurrentlyInView].projectColor
+      );
+    } else {
+      const lastProject = PROJECTS.length - 1;
+      setActiveProjectColor(PROJECTS[lastProject].projectColor);
+    }
+  };
 
   return (
     <>
-      <GridLines projectColor={projectColor} />
+      <GridLines projectColor={activeProjectColor} />
       <NavBar
-        projectColor={projectColor}
+        projectColor={activeProjectColor}
         activeTranslationBtn={activeTranslationBtn}
         handleOnClickSpanishBtn={handleOnClickSpanishBtn}
         handleOnClickEnglishBtn={handleOnClickEnglishBtn}
         translation={translation}
       />
-      <Home translation={translation} />
+      <Home translation={translation} projects={PROJECTS} />
     </>
   );
 };

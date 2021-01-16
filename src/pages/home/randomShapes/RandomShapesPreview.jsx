fd3256659d6179b284174a28abe1c00af3d1df97
Sean Dorr <sean@nominapress.es>
2021-01-16 22:11:13 +0100

    Creating logic to reload the component by changing state

--- src/pages/home/randomShapes/RandomShapesPreview.jsx
+++ src/pages/home/randomShapes/RandomShapesPreview.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GenerateRandomShapes } from "./components/GenerateRandomShapes";
 import "./random-shapes.scss";
 
 const RandomShapesPreview = () => {
+  const [, setReloadComponent] = useState(undefined);
+
+  useEffect(() => {
+    setReloadComponent({});
+  }, []);
+
   return (
     <div className="random-shapes__container">
       <GenerateRandomShapes projectPreview />

8a29a924452fe92e0f2748936fc360593c8f9e4d
Sean Dorr <sean@nominapress.es>
2021-01-16 19:41:27 +0100

    Adding projectColor with deconstructed scss variable

--- src/pages/home/utils/constants/projectData.js
+++ src/pages/home/utils/constants/projectData.js
@@ -1,23 +1,27 @@
 import React from "react";
 import RandomShapesPreview from "../../randomShapes/RandomShapesPreview";
+import colors from "../../../../styles/_colors.scss";
 
 const projectData = {
   randomShapes: {
     projectId: 1,
     projectName: "randomShapes",
     projectComponent: <RandomShapesPreview />,
+    projectColor: colors.randomShapesColor,
     projectLink: "#",
   },
   gemini: {
     projectId: 2,
     projectName: "gemini",
     projectImage: "gemini-hero.jpg",
+    projectColor: colors.geminiColor,
     projectLink: "#",
   },
   targetbase: {
     projectId: 3,
     projectName: "targetbase",
     projectImage: "targetbase-hero.jpg",
+    projectColor: colors.targetbaseColor,
     projectLink: "#",
   },
 };

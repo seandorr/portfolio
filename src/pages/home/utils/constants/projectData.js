05f7934b7f891c8afb14cde719621c4786333323
Sean Dorr <sean@nominapress.es>
2021-01-16 16:24:24 +0100

    Adding new project with component instead of image

--- src/pages/home/utils/constants/projectData.js
+++ src/pages/home/utils/constants/projectData.js
@@ -1,12 +1,21 @@
+import React from "react";
+import RandomShapesPreview from "../../randomShapes/RandomShapesPreview";
+
 const projectData = {
-  gemini: {
+  randomShapes: {
     projectId: 1,
+    projectName: "randomShapes",
+    projectComponent: <RandomShapesPreview />,
+    projectLink: "#",
+  },
+  gemini: {
+    projectId: 2,
     projectName: "gemini",
     projectImage: "gemini-hero.jpg",
     projectLink: "#",
   },
   targetbase: {
-    projectId: 2,
+    projectId: 3,
     projectName: "targetbase",
     projectImage: "targetbase-hero.jpg",
     projectLink: "#",

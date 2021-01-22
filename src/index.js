8b1a3bb768e0c5adc11b2b2d077751952dcfe326
Sean Dorr <sean@nominapress.es>
2021-01-22 23:12:20 +0100

    Removing router and moving to App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
@@ -22,10 +21,8 @@ i18next.init({
 });
 
 ReactDOM.render(
-  <Router>
-    <I18nextProvider i18n={i18next}>
-      <App />
-    </I18nextProvider>
-  </Router>,
+  <I18nextProvider i18n={i18next}>
+    <App />
+  </I18nextProvider>,
   document.getElementById("root")
 );

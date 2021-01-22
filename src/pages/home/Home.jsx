a5948d7d71ce74535a8b71995eebc0164995a52d
Sean Dorr <sean@nominapress.es>
2021-01-22 23:15:33 +0100

    Moving active project color logic here

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,55 @@
-import React, { Suspense } from "react";
+import React, { useEffect, Suspense } from "react";
 import ProjectPreview from "./projectPreview/ProjectPreview";
+import useWindowSize from "../../utils/customHooks/useWindowSize";
+import useScrollDirection from "../../utils/customHooks/useScrollDirection";
 import "./home.scss";
 
 const Home = (props) => {
-  const { translation, projects } = props;
+  const { translation, projects, setActiveProjectColor } = props;
+
+  const getScrollDirection = useScrollDirection();
+  const getWindowHeight = useWindowSize().height;
+  const getWindowWidth = useWindowSize().width;
+
+  let calculatedWindowHeight;
+  if (getScrollDirection === "down" && getWindowWidth >= 768) {
+    calculatedWindowHeight = getWindowHeight - 100;
+  } else if (getScrollDirection === "up" && getWindowWidth >= 768) {
+    calculatedWindowHeight = getWindowHeight - 200;
+  } else {
+    calculatedWindowHeight = 800;
+  }
+
+  const handleScroll = () => {
+    const indexOfProjectCurrentlyInView = Math.floor(
+      window.scrollY / calculatedWindowHeight
+    );
+
+    if (indexOfProjectCurrentlyInView < projects.length) {
+      setActiveProjectColor(
+        projects[indexOfProjectCurrentlyInView].projectColor
+      );
+    } else {
+      const lastProject = projects.length - 1;
+      setActiveProjectColor(projects[lastProject].projectColor);
+    }
+  };
+
+  const handleProjectColors = () => {
+    if (window.scrollY <= 1) {
+      setActiveProjectColor(projects[0].projectColor);
+    } else {
+      window.addEventListener("scroll", handleScroll);
+    }
+  };
+
+  const handleOnLoad = () => {
+    handleProjectColors();
+  };
+
+  useEffect(() => {
+    window.onload = handleOnLoad();
+  }, [handleOnLoad]);
 
   return (
     <Suspense fallback="loading">

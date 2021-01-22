02e9b54da80a8353341f0cc7a3feab449ef8747b
Sean Dorr <sean@nominapress.es>
2021-01-22 23:17:12 +0100

    Removing useEffect import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import GridLines from "./components/gridLines/GridLines";

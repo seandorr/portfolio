ca6d8a112f48927747b671f7ec84cf3e72fb0758
Sean Dorr <sean@nominapress.es>
2021-01-22 23:16:03 +0100

    Adding basic styles to test activeProjectColor

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const About = () => {
-  return <div>about</div>;
+const About = ({ setActiveProjectColor }) => {
+  setActiveProjectColor("green");
+  return <div style={{ height: "300vh" }}>about</div>;
 };
 
 export default About;
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Contact = () => {
-  return <div>Contact</div>;
+const Contact = ({ setActiveProjectColor }) => {
+  setActiveProjectColor("red");
+  return <div style={{ height: "300vh" }}>contact</div>;
 };
 
 export default Contact;

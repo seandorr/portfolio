082d00c9c98dda1a25cfa8c08456a4496556ee79
Sean Dorr <sean@nominapress.es>
2021-03-13 22:20:58 +0100

    Adding content and styles for about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,38 @@
 import React, { useEffect } from "react";
+import FilterTags from "../../components/FilterTags/FilterTags";
+import "./about.scss";
 
-const About = ({ setActiveProjectColor }) => {
+const About = ({ translation, setActiveProjectColor }) => {
   useEffect(() => {
-    setActiveProjectColor("green");
+    setActiveProjectColor("#0C4C7A");
   }, [setActiveProjectColor]);
 
-  return <div style={{ height: "300vh" }}>about</div>;
+  return (
+    <div className="about-container">
+      <img
+        className="about-grid-item headshot"
+        src="/images/sean-headshot2.jpeg"
+      />
+      <div className="about-grid-item tags">
+        <FilterTags translation={translation} location="about" />
+      </div>
+      <div className="about-grid-item about-text">
+        <p>
+          Hi, I’m Sean. I'm a designer and front-end developer based in Madrid,
+          España. I'm currently seeking opportunities in either of these areas,
+          or ideally, a combination of both. I love to create memorable
+          experiences through the use of branding and web design. In my humble
+          opinion, too many brands are following the same 'corporate' layout and
+          making similar strides as their competition. I aim to break down these
+          barriers in a mindful way that highlight the importance of the brand.
+          I'm a life-long student and am always trying to learn new techniques,
+          technologies, and languages. When I'm not working or learning, you can
+          catch me hiking the great outdoors, curating Spotify playlists, and
+          sparking conversations with strangers.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;

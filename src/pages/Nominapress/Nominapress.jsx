898441fc02ae6d6e2e17114ddfc649a24e1edf92
Sean Dorr <seanmichael.dorr@gmail.com>
2021-05-15 23:06:40 +0200

    Creating basic slideshow

--- src/pages/Nominapress/Nominapress.jsx
+++ src/pages/Nominapress/Nominapress.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import FacialRecogMgmt from "./assets/facial-recog-mgmt.png";
 import "./nominapress.scss";
+import { facialRecogImages } from "./utils/facialRecogSliderImages";
 
 const Nominapress = () => {
+  const [activeImgIndex, setActiveImgIndex] = useState(0);
+  const allImages = facialRecogImages;
+
+  const handleNextButton = () => {
+    if (allImages.length - 1 === activeImgIndex) {
+      setActiveImgIndex(0);
+    } else {
+      setActiveImgIndex(activeImgIndex + 1);
+    }
+  };
+
   return (
     <div className="nominapress-container">
-      <img className="nominapress-img" src={FacialRecogMgmt} />
+      <img
+        className="nominapress-img"
+        src={`images/nominapress/${facialRecogImages[activeImgIndex]}`}
+      />
+      <button onClick={handleNextButton}>next</button>
     </div>
   );
 };

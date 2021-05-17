71847265e155b6af4ca3420b82d2008e46fed367
Sean Dorr <sean@nominapress.es>
2021-05-17 21:51:24 +0200

    Adding another slider with new content info

--- src/pages/Nominapress/Nominapress.jsx
+++ src/pages/Nominapress/Nominapress.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./nominapress.scss";
-import { facialRecogImages } from "./utils/facialRecogSliderImages";
+import {
+  facialRecogValidationImages,
+  facialRecogLoginImage,
+} from "./utils/facialRecogImages";
 import Slideshow from "../../components/Slideshow/Slideshow";
+import useTranslation from "../../utils/customHooks/useTranslation";
 
 const Nominapress = ({ projectColor }) => {
+  const { translation } = useTranslation();
   return (
-    <Slideshow
-      imageArray={facialRecogImages}
-      projectDirectory="nominapress"
-      title="Facial Recog"
-      projectColor={projectColor}
-    />
+    <div className="nominapress-grid">
+      <Slideshow
+        className="nominapress-grid-item"
+        imageArray={facialRecogValidationImages}
+        projectDirectory="nominapress"
+        title={translation("project.nominapress.facialRecogValidationSlider")}
+        projectColor={projectColor}
+      />
+      <Slideshow
+        className="nominapress-grid-item"
+        imageArray={facialRecogLoginImage}
+        projectDirectory="nominapress"
+        title={translation("project.nominapress.facialRecogLoginSlider")}
+        projectColor={projectColor}
+      />
+    </div>
   );
 };
 

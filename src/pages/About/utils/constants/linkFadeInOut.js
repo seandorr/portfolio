52b051eb24eac1b1efe75189f686ecd90268805a
Sean Dorr <seanmichael.dorr@gmail.com>
2021-05-26 23:01:50 +0200

    Creating object of animation

---
+++ src/pages/About/utils/constants/linkFadeInOut.js
@@ -0,0 +1,17 @@
+export const linkFadeInOut = {
+  initial: {
+    opacity: 0,
+    y: 20,
+    x: "-50%",
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    x: "-50%",
+  },
+  exit: {
+    opacity: 0,
+    y: 20,
+    x: "-50%",
+  },
+};

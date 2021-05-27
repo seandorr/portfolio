029263f4a2eb9cded36b2bdbd5bcdd8cd425ee8d
Sean Dorr <seanmichael.dorr@gmail.com>
2021-05-27 20:48:19 +0200

    Calling new components on main
    Adding wrapper

--- src/pages/Gemini/Gemini.jsx
+++ src/pages/Gemini/Gemini.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import GeminiHero from "./components/Hero/GeminiHero";
-import Tech from "./components/icons/Tech/Tech";
-import Safe from "./components/icons/Safe/Safe";
-import Portal from "./components/icons/Portal/Portal";
+import GeminiHero from "./components/GeminiHero/GeminiHero";
+import GeminiIcons from "./components/GeminiIcons/GeminiIcons";
 
 const Gemini = (props) => {
   return (
-    <>
+    <div className="gemini-main-container">
       <GeminiHero />
-      <Tech />
-      <Safe />
-      <Portal />
-    </>
+      <GeminiIcons />
+    </div>
   );
 };
 

d65e061f3d25c3e5515b4403ac7037610ae1941b
Sean Dorr <seanmichael.dorr@gmail.com>
2026-07-26 17:19:41 +0200

    fix: replace figma embed on small screens

--- src/pages/AIChina/components/AIChinaSections.jsx
+++ src/pages/AIChina/components/AIChinaSections.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import styles from "../ai-in-china.module.scss";
+import useWindowSize from "../../../utils/customHooks/useWindowSize";
 
 export const UserJourneySection = ({ type, title, src, description }) => {
+  const browserWidth = useWindowSize().width;
+  const smallScreen = React.useMemo(() => browserWidth < 768, [browserWidth]);
+
   return (
     <section className={styles.userJourneySection}>
       {type === "proposal" ? (
@@ -9,15 +13,23 @@ export const UserJourneySection = ({ type, title, src, description }) => {
       ) : (
         <h3 className={styles.aiChinaSubtitle}>{title}:</h3>
       )}
-
-      {src && (
-        <iframe
-          title={title}
-          className={styles.aiChinaFigmaEmbed}
-          src={src}
-          allowFullScreen
-        ></iframe>
+      {smallScreen ? (
+        <div className={styles.placeholder}>
+          <span className={styles.placeholderText}>
+            Figma preview will appear here on larger screens.
+          </span>
+        </div>
+      ) : (
+        src && (
+          <iframe
+            title={title}
+            className={styles.aiChinaFigmaEmbed}
+            src={src}
+            allowFullScreen
+          ></iframe>
+        )
       )}
+
       <p className={styles.aiChinaDescription}>{description}</p>
     </section>
   );
